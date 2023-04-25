2bcdc6fc10b84b33a3d2eb3bfd9920ee0da7b98d
smellslikeml <trollofberkeley@gmail.com>
2023-04-25 07:39:01 -0700

    added pretty print to cli output

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="remyxai",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["requests"],
     entry_points={
