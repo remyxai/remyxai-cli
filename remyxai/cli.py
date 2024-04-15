94f6951a9b4cb3d0a5b544e2abe94d7d25dce14d
smellslikeml <terry.j.rodriguez@gmail.com>
2024-04-15 06:12:13 +0000

    adding deployment for generate models

--- remyxai/cli.py
+++ remyxai/cli.py
@@ -1,70 +1,67 @@
 from .api import *
 from .utils import labeler
 import argparse
+import time
 from pprint import pprint
+import subprocess
 
 def main():
     parser = argparse.ArgumentParser(description="Model management script")
 
-    # Define top-level actions
     subparsers_action = parser.add_subparsers(dest="action", help="Top-level actions")
 
-    # Define 'model' action
     model_parser = subparsers_action.add_parser("model", help="Model-related actions")
 
-    # Define subactions for 'model' action
     subparsers_model = model_parser.add_subparsers(dest="subaction", help="Model subactions")
 
-    # Define 'list' subaction
     list_parser = subparsers_model.add_parser("list", help="List your models")
 
-    # Define 'summary' subaction
     summary_parser = subparsers_model.add_parser("summarize", help="Summarize a model")
     summary_parser.add_argument("--model_name", required=True, help="Name of the model to provide a summary")
 
-    # Define 'download' subaction
     download_parser = subparsers_model.add_parser("download", help="Download and convert a model")
     download_parser.add_argument("--model_name", required=True, help="Name of the model to delete")
     download_parser.add_argument("--model_format", required=True, help="of the model to delete")
 
-    # Define 'delete' subaction
     delete_parser = subparsers_model.add_parser("delete", help="Delete a model")
     delete_parser.add_argument("--model_name", required=True, help="Name of the model to delete")
 
-    # Define 'classify' action
     classify_parser = subparsers_action.add_parser("classify", help="Classifier-related actions")
     classify_parser.add_argument("--model_name", required=True, help="Name of the model")
     classify_parser.add_argument("--labels", required=True, help="Comma separated list of labels, e.g. 'cat,dog'")
     classify_parser.add_argument("--model_size", required=True, help="Integer value for model size from 1=small up to 5=large")
     classify_parser.add_argument("--hf_dataset", required=False, default=None, help="(Optional) Name of the HuggingFace dataset to use for training")
 
-    # Define 'detect' action
     detect_parser = subparsers_action.add_parser("detect", help="Detector-related actions")
     detect_parser.add_argument("--model_name", required=True, help="Name of the model")
     detect_parser.add_argument("--labels", required=True, help="Comma separated list of labels, e.g. 'cat,dog'")
     detect_parser.add_argument("--model_size", required=True, help="Integer value for model size from 1=small up to 5=large")
     detect_parser.add_argument("--hf_dataset", required=False, default=None, help="(Optional) Name of the HuggingFace dataset to use for training")
 
-    # Define 'generate' action
     generate_parser = subparsers_action.add_parser("generate", help="Generator-related actions")
     generate_parser.add_argument("--model_name", required=True, help="Name of the model")
     generate_parser.add_argument("--hf_dataset", required=True, default=None, help="Name of the HuggingFace dataset to use for training")
 
-    # Define 'user' action
+    deploy_parser = subparsers_action.add_parser("deploy", help="Deploy or tear down a model using Docker Compose")
+    deploy_parser.add_argument("--model_name", required=True, help="Name of the model to deploy or tear down")
+    deploy_parser.add_argument("command", nargs='?', default="up", choices=['up', 'down'], help="Specify 'up' to deploy or 'down' to tear down the model")
+
+    infer_parser = subparsers_action.add_parser("infer", help="Submit an inference request to Triton server")
+    infer_parser.add_argument("--model_name", type=str, required=True, help="Model name for the inference")
+    infer_parser.add_argument("--prompt", type=str, required=True, help="Prompt to be used for the inference")
+    infer_parser.add_argument("--server_url", type=str, default="localhost:8000", help="URL of the Triton Inference Server")
+    infer_parser.add_argument("--model_version", type=str, default="1", help="Version of the model")
+
     user_parser = subparsers_action.add_parser("user", help="User-related actions")
 
-    # Define subactions for 'user' action
     subparsers_user = user_parser.add_subparsers(dest="subaction", help="User subactions")
     profile_parser = subparsers_user.add_parser("profile", help="Get user profile")
     credits_parser = subparsers_user.add_parser("credits", help="Get user credits and subscription info if it exists")
     
-    # Define 'utils' action
     utils_parser = subparsers_action.add_parser("utils", help="Utility actions")
     
-    # Define subactions for 'utils' action
     subparsers_utils = utils_parser.add_subparsers(dest="subaction", help="Utility subactions")
 
-    # Define 'labeler' subaction
     labeler_parser = subparsers_utils.add_parser("label", help="Extract labels from a directory of images")
     labeler_parser.add_argument("--labels", required=True, help="Comma separated list of labels, e.g. 'cat,dog'")
     labeler_parser.add_argument("--image_dir", required=True, help="Directory of images in '.jpg', '.jpeg', '.png' format")
@@ -103,6 +100,14 @@ def main():
     elif args.action == "generate":                                                                            
         training_generator = train_generator(args.model_name, args.hf_dataset)
         pprint(training_generator)
+
+    elif args.action == "deploy":
+        deploy_model(args.model_name, args.command)
+
+    elif args.action == "infer":
+        result, time_elapsed = run_inference(args.model_name, args.prompt, args.server_url, args.model_version)
+        print("--- %s seconds ---" % time_elapsed)
+        print(result)
 
     elif args.action == "user":
         if args.subaction == "profile":
