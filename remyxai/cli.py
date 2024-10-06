51867279aa12d5fd43390a40732d84eb2277a7ee
harshbhatt7585 <harshbhatt7585@gmail.com>
2024-10-06 19:47:45 +0000

    Setup Pre-commit yaml with black formatting

--- remyxai/cli.py
+++ remyxai/cli.py
@@ -1,5 +1,3 @@
-
-
 import argparse
 import time
 import logging
@@ -8,6 +6,7 @@ import json
 import sys
 from actions import *
 
+
 def main():
     parser = argparse.ArgumentParser(description="Model management script")
 
@@ -15,59 +14,145 @@ def main():
 
     model_parser = subparsers_action.add_parser("model", help="Model-related actions")
 
-    subparsers_model = model_parser.add_subparsers(dest="subaction", help="Model subactions")
+    subparsers_model = model_parser.add_subparsers(
+        dest="subaction", help="Model subactions"
+    )
 
     list_parser = subparsers_model.add_parser("list", help="List your models")
 
     summary_parser = subparsers_model.add_parser("summarize", help="Summarize a model")
-    summary_parser.add_argument("--model_name", required=True, help="Name of the model to provide a summary")
+    summary_parser.add_argument(
+        "--model_name", required=True, help="Name of the model to provide a summary"
+    )
 
-    download_parser = subparsers_model.add_parser("download", help="Download and convert a model")
-    download_parser.add_argument("--model_name", required=True, help="Name of the model to delete")
-    download_parser.add_argument("--model_format", required=True, help="of the model to delete")
+    download_parser = subparsers_model.add_parser(
+        "download", help="Download and convert a model"
+    )
+    download_parser.add_argument(
+        "--model_name", required=True, help="Name of the model to delete"
+    )
+    download_parser.add_argument(
+        "--model_format", required=True, help="of the model to delete"
+    )
 
     delete_parser = subparsers_model.add_parser("delete", help="Delete a model")
-    delete_parser.add_argument("--model_name", required=True, help="Name of the model to delete")
+    delete_parser.add_argument(
+        "--model_name", required=True, help="Name of the model to delete"
+    )
 
-    classify_parser = subparsers_action.add_parser("classify", help="Classifier-related actions")
-    classify_parser.add_argument("--model_name", required=True, help="Name of the model")
-    classify_parser.add_argument("--labels", required=True, help="Comma separated list of labels, e.g. 'cat,dog'")
-    classify_parser.add_argument("--model_size", required=True, help="Integer value for model size from 1=small up to 5=large")
-    classify_parser.add_argument("--hf_dataset", required=False, default=None, help="(Optional) Name of the HuggingFace dataset to use for training")
+    classify_parser = subparsers_action.add_parser(
+        "classify", help="Classifier-related actions"
+    )
+    classify_parser.add_argument(
+        "--model_name", required=True, help="Name of the model"
+    )
+    classify_parser.add_argument(
+        "--labels", required=True, help="Comma separated list of labels, e.g. 'cat,dog'"
+    )
+    classify_parser.add_argument(
+        "--model_size",
+        required=True,
+        help="Integer value for model size from 1=small up to 5=large",
+    )
+    classify_parser.add_argument(
+        "--hf_dataset",
+        required=False,
+        default=None,
+        help="(Optional) Name of the HuggingFace dataset to use for training",
+    )
 
-    detect_parser = subparsers_action.add_parser("detect", help="Detector-related actions")
+    detect_parser = subparsers_action.add_parser(
+        "detect", help="Detector-related actions"
+    )
     detect_parser.add_argument("--model_name", required=True, help="Name of the model")
-    detect_parser.add_argument("--labels", required=True, help="Comma separated list of labels, e.g. 'cat,dog'")
-    detect_parser.add_argument("--model_size", required=True, help="Integer value for model size from 1=small up to 5=large")
-    detect_parser.add_argument("--hf_dataset", required=False, default=None, help="(Optional) Name of the HuggingFace dataset to use for training")
+    detect_parser.add_argument(
+        "--labels", required=True, help="Comma separated list of labels, e.g. 'cat,dog'"
+    )
+    detect_parser.add_argument(
+        "--model_size",
+        required=True,
+        help="Integer value for model size from 1=small up to 5=large",
+    )
+    detect_parser.add_argument(
+        "--hf_dataset",
+        required=False,
+        default=None,
+        help="(Optional) Name of the HuggingFace dataset to use for training",
+    )
 
-    generate_parser = subparsers_action.add_parser("generate", help="Generator-related actions")
-    generate_parser.add_argument("--model_name", required=True, help="Name of the model")
-    generate_parser.add_argument("--hf_dataset", required=True, default=None, help="Name of the HuggingFace dataset to use for training")
+    generate_parser = subparsers_action.add_parser(
+        "generate", help="Generator-related actions"
+    )
+    generate_parser.add_argument(
+        "--model_name", required=True, help="Name of the model"
+    )
+    generate_parser.add_argument(
+        "--hf_dataset",
+        required=True,
+        default=None,
+        help="Name of the HuggingFace dataset to use for training",
+    )
 
-    deploy_parser = subparsers_action.add_parser("deploy", help="Deploy or tear down a model using Docker Compose")
-    deploy_parser.add_argument("--model_name", required=True, help="Name of the model to deploy or tear down")
-    deploy_parser.add_argument("command", nargs='?', default="up", choices=['up', 'down'], help="Specify 'up' to deploy or 'down' to tear down the model")
+    deploy_parser = subparsers_action.add_parser(
+        "deploy", help="Deploy or tear down a model using Docker Compose"
+    )
+    deploy_parser.add_argument(
+        "--model_name", required=True, help="Name of the model to deploy or tear down"
+    )
+    deploy_parser.add_argument(
+        "command",
+        nargs="?",
+        default="up",
+        choices=["up", "down"],
+        help="Specify 'up' to deploy or 'down' to tear down the model",
+    )
 
-    infer_parser = subparsers_action.add_parser("infer", help="Submit an inference request to Triton server")
-    infer_parser.add_argument("--model_name", type=str, required=True, help="Model name for the inference")
-    infer_parser.add_argument("--prompt", type=str, required=True, help="Prompt to be used for the inference")
-    infer_parser.add_argument("--server_url", type=str, default="localhost:8000", help="URL of the Triton Inference Server")
-    infer_parser.add_argument("--model_version", type=str, default="1", help="Version of the model")
+    infer_parser = subparsers_action.add_parser(
+        "infer", help="Submit an inference request to Triton server"
+    )
+    infer_parser.add_argument(
+        "--model_name", type=str, required=True, help="Model name for the inference"
+    )
+    infer_parser.add_argument(
+        "--prompt", type=str, required=True, help="Prompt to be used for the inference"
+    )
+    infer_parser.add_argument(
+        "--server_url",
+        type=str,
+        default="localhost:8000",
+        help="URL of the Triton Inference Server",
+    )
+    infer_parser.add_argument(
+        "--model_version", type=str, default="1", help="Version of the model"
+    )
 
     user_parser = subparsers_action.add_parser("user", help="User-related actions")
 
-    subparsers_user = user_parser.add_subparsers(dest="subaction", help="User subactions")
+    subparsers_user = user_parser.add_subparsers(
+        dest="subaction", help="User subactions"
+    )
     profile_parser = subparsers_user.add_parser("profile", help="Get user profile")
-    credits_parser = subparsers_user.add_parser("credits", help="Get user credits and subscription info if it exists")
-    
-    utils_parser = subparsers_action.add_parser("utils", help="Utility actions")
-    
-    subparsers_utils = utils_parser.add_subparsers(dest="subaction", help="Utility subactions")
+    credits_parser = subparsers_user.add_parser(
+        "credits", help="Get user credits and subscription info if it exists"
+    )
 
-    labeler_parser = subparsers_utils.add_parser("label", help="Extract labels from a directory of images")
-    labeler_parser.add_argument("--labels", required=True, help="Comma separated list of labels, e.g. 'cat,dog'")
-    labeler_parser.add_argument("--image_dir", required=True, help="Directory of images in '.jpg', '.jpeg', '.png' format")
+    utils_parser = subparsers_action.add_parser("utils", help="Utility actions")
+
+    subparsers_utils = utils_parser.add_subparsers(
+        dest="subaction", help="Utility subactions"
+    )
+
+    labeler_parser = subparsers_utils.add_parser(
+        "label", help="Extract labels from a directory of images"
+    )
+    labeler_parser.add_argument(
+        "--labels", required=True, help="Comma separated list of labels, e.g. 'cat,dog'"
+    )
+    labeler_parser.add_argument(
+        "--image_dir",
+        required=True,
+        help="Directory of images in '.jpg', '.jpeg', '.png' format",
+    )
     labeler_parser.add_argument("--model_name", default=None, help="Name of the model")
 
     args = parser.parse_args()
@@ -90,7 +175,6 @@ def main():
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
         sys.exit(1)
-
 
 
 if __name__ == "__main__":
--- remyxai/utils.py
+++ remyxai/utils.py
@@ -17,17 +17,21 @@ import logging
 
 # Utilities
 
+
 def load_image(image_path, img_height=224, img_width=224):
-    img = Image.open(image_path).convert('RGB')
+    img = Image.open(image_path).convert("RGB")
     img = img.resize([img_height, img_width])
 
-    img = np.asarray(img, dtype='float32') / 255
+    img = np.asarray(img, dtype="float32") / 255
     # Return a scaled array between -1 and 1
     return img * 2 - 1
 
+
 class RemyxModel(object):
     def __init__(self, model_assets_path):
-        self.model_file = [x for x in os.listdir(model_assets_path) if x.endswith(".onnx")][0]
+        self.model_file = [
+            x for x in os.listdir(model_assets_path) if x.endswith(".onnx")
+        ][0]
         self.model_path = os.path.join(model_assets_path, self.model_file)
         metadata_file = os.path.join(model_assets_path, "metadata.csv")
 
@@ -60,41 +64,44 @@ class RemyxModel(object):
         preds = self.model.run([self.output_name], {self.input_name: img})[0]
         pred_idx = np.argmax(preds, axis=1)
         pred_classes = np.take(self.labels, pred_idx, axis=0).tolist()
-        results = [{"file": f, "label": l} for f,l in list(zip(inputs, pred_classes))]
+        results = [{"file": f, "label": l} for f, l in list(zip(inputs, pred_classes))]
         return results
 
     def predict(self, inputs):
         return self(inputs)
 
+
 def process_images_in_directory(directory, processor, chunk_size=10):
     image_files = []
-    
+
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith((".jpg", ".jpeg", ".png")):
                 image_files.append(os.path.join(root, file))
-    
+
     results = []
-    if os.path.isfile('processed_results.json'):
-        with open('processed_results.json', 'r') as json_file:
+    if os.path.isfile("processed_results.json"):
+        with open("processed_results.json", "r") as json_file:
             results = json.load(json_file)
 
-    for i in tqdm(range(0, len(image_files), chunk_size), desc='Processing images'):
-        chunk = image_files[i:i+chunk_size]
+    for i in tqdm(range(0, len(image_files), chunk_size), desc="Processing images"):
+        chunk = image_files[i : i + chunk_size]
         processed_chunk = processor(chunk)
         results.extend(processed_chunk)
 
-        with open('processed_results.json', 'w') as json_file:
+        with open("processed_results.json", "w") as json_file:
             json.dump(results, json_file)
-            
-        logging.info(f"Processed {min(i+chunk_size, len(image_files))} out of {len(image_files)} images")
-        
-    return 'Results are stored in ./processed_results.json'
+
+        logging.info(
+            f"Processed {min(i+chunk_size, len(image_files))} out of {len(image_files)} images"
+        )
+
+    return "Results are stored in ./processed_results.json"
 
 
 def labeler(labels: list, image_dir: str, model_name=None):
     """
-    Wrapper to train a custom image classifier, 
+    Wrapper to train a custom image classifier,
     download model, and run inference on a folder of images.
     """
     # Step 1: Train a model if it doesn't exist
@@ -108,7 +115,7 @@ def labeler(labels: list, image_dir: str, model_name=None):
 
     while status != "FINISHED":
         status = get_model_summary(model_name)["message"][0]["status"]
-        logging.info(f'Current model status: {status}')
+        logging.info(f"Current model status: {status}")
         if status == "RUNNING":
             # Check back in 5 minutes:
             time.sleep(300)
@@ -121,11 +128,11 @@ def labeler(labels: list, image_dir: str, model_name=None):
     download_message = download_model(model_name=model_name, model_format="onnx")
 
     logging.info(download_message)
-    logging.info("Preparing for inference...")                                                                                                                                                                     
-    # Step 2: Decompress model assets, load onnx model  
+    logging.info("Preparing for inference...")
+    # Step 2: Decompress model assets, load onnx model
     model_assets_path = os.path.join(os.getcwd(), model_name)
     model_assets_zip = model_assets_path + ".zip"
-    with zipfile.ZipFile(model_assets_zip,"r") as zip_ref:
+    with zipfile.ZipFile(model_assets_zip, "r") as zip_ref:
         zip_ref.extractall(model_assets_path)
     model = RemyxModel(model_assets_path)
 
