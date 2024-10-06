6dcfb3ce7f08d45da183b736f307a9246e915bf7
salma-remyx <salma@remyx.ai>
2024-10-06 19:23:50 +0000

    include hf test

--- tests/myxboard/test_myxboard.py
+++ tests/myxboard/test_myxboard.py
@@ -9,38 +9,58 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(mes
 
 def test_myxboard_evaluation():
     try:
-        # Step 1: Initialize MyxBoard with a list of model identifiers
         model_ids = ["Phi-3-mini-4k-instruct", "Qwen2-1.5B"]
-        myx_board_name = "test_myxboard_new_3"
+        myx_board_name = "test_myxboard"
         myx_board = MyxBoard(model_repo_ids=model_ids, name=myx_board_name)
         logging.info(f"MyxBoard {myx_board_name} initialized with models: {model_ids}")
 
-        # Step 2: Initialize the evaluation API client
         remyx_api = RemyxAPI()
         logging.info("Initialized RemyxAPI client")
 
-        # Step 3: Define the tasks you want to evaluate
         tasks = [EvaluationTask.MYXMATCH]
-        prompt = "What are 2 characteristics of a good employee?"
+        prompt = "What are 2 characteristics of a good coder?"
 
-        # Step 4: Evaluate the entire MyxBoard on all specified tasks
         remyx_api.evaluate(myx_board, tasks, prompt=prompt)
         logging.info(f"Evaluation started for MyxBoard {myx_board_name} with tasks: {tasks}")
 
-        # Step 5: Fetch the updated results from the MyxBoard
         results = myx_board.get_results()
         logging.info(f"Results fetched for MyxBoard {myx_board_name}: {results}")
 
-        # Step 6: Print the results
         print("Evaluation Results:")
         print(results)
 
     except Exception as e:
-        # Log the full traceback in case of any exception
         logging.error(f"An error occurred during the MyxBoard evaluation test: {str(e)}")
-        logging.error(traceback.format_exc())  # Log the full traceback for detailed debugging
+        logging.error(traceback.format_exc())
 
-# Run the test function
+def test_myxboard_evaluationi_hf(collection_name):
+    try:
+        myx_board =  MyxBoard(hf_collection_name=collection_name)
+        logging.info(f"MyxBoard {myx_board_name} initialized from collection: {collection_name}")
+
+        remyx_api = RemyxAPI()
+        logging.info("Initialized RemyxAPI client")
+
+        tasks = [EvaluationTask.MYXMATCH]
+        prompt = "What are 2 characteristics of a good coder?"
+
+        remyx_api.evaluate(myx_board, tasks, prompt=prompt)
+        logging.info(f"Evaluation started for MyxBoard {myx_board_name} with tasks: {tasks}")
+
+        results = myx_board.get_results()
+        logging.info(f"Results fetched for MyxBoard {myx_board_name}: {results}")
+
+        print("Evaluation Results:")
+        print(results)
+
+        # Optional: upload results to collection
+        myx_board.push_to_hf()
+
+
+    except Exception as e:
+        logging.error(f"An error occurred during the MyxBoard evaluation test: {str(e)}")
+        logging.error(traceback.format_exc())
+
 if __name__ == "__main__":
     test_myxboard_evaluation()
 
