0e54bf410c36c12b197ce7d17c07d1d890988f69
salma-remyx <salma@remyx.ai>
2026-03-22 00:32:20 +0000

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="remyxai",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(include=["remyxai", "remyxai.*"]),
     install_requires=[
         "numpy",
